64eef6691889a03a535f315a7f125769477ba3ca
Edoardo <colonna17@gmail.com>
2023-12-16 17:46:47 +0100

    Transiton func

--- models/Transition.py
+++ models/Transition.py
@@ -20,10 +20,14 @@ class TransitionModel(nn.Module):
         self.stochastic_sz= stochastic_sz
         self.deterministic_sz = deterministic_sz
         self.hidden_sz = hidden_sz
-        self._activation = nn.ELU
-        self.lstm = nn.GRUCell(hidden_sz, deterministic_sz)
+        self.activation = nn.ELU
+        self.gru = nn.GRUCell(hidden_sz, deterministic_sz)
+        self.linear1 = nn.Linear(self.action_sz + self.stochastic_sz, self.hidden_sz)
         #self._rnn_input_model = self._build_rnn_input_model()
-        #self._stochastic_prior_model = self._build_stochastic_model()
+        self.stochastic_model = nn.Sequential(nn.Linear(self.hidden_sz,self.hidden_sz),
+                                              self.activation(),
+                                              nn.Linear(self.hidden_sz, self.hidden_sz *2),
+                                              self.activation())
         self._dist = distribution
 
     def initial_state(self, batch_sz, ):
@@ -37,7 +41,18 @@ class TransitionModel(nn.Module):
 
 
     def forward(self, prev_state, prev_action):
-        input = nn.Sequential(nn.Linear(self.action_sz + self.stochastic_sz, self.hidden_sz),
-                              self._activation())
+        prev_input = self.activation(self.linear1(torch.cat([prev_action, prev_state.stoch], dim=-1)))
+        # In GRUcell we pass previous  stochastic state and action as input features and
+        # previous deterministic state as previous hidden state of the rnn
+        deterministic_state = self.gru(prev_input, prev_state.deter)
+        mean, std = torch.chunk(self.stochastic_model(deterministic_state), 2, dim=-1)
+        std = tf.softplus(std) + 0.1
+        dist = self.distribution(mean, std) #Normal distribution
+        stochastic_state = dist.rsample()
+        #stochastic_state = dist.sample()
+        return RSSMState(mean, std, stochastic_state, deterministic_state)
+
+
+        
         
 
